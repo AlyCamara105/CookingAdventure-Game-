14539405cf071b0cbbdb4e081381d668daf63f70
Mystiogan <frodo.marxer@gmx.ch>
2021-05-22 19:51:15 +0200

    Clone Commit

---
+++ src/Server/Services/TestService.lua
@@ -0,0 +1,21 @@
+-- Test Service
+-- Username
+-- May 22, 2021
+
+
+
+local TestService = {Client = {}}
+
+-- this is a comment
+
+function TestService:Start()
+	
+end
+
+
+function TestService:Init()
+	
+end
+
+
+return TestService
